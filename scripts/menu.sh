eb0b2a02972c9f9cf2deec8f4d2d323a709c6a93
GComputeNerd <GComputeNerd@users.noreply.github.com>
2022-11-02 09:23:32 +0530

    Added colors to menu

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -6,7 +6,27 @@ LINKS="$CURRENT_DIR/../links/"
 a=($(ls $LINKS)) # Gets list of bookmarks
 lim=${#a[@]}
 i=0
-cmd=('whiptail' '--title "Hello THere!"' '--menu Hello 20 60 10')
+cmd=("NEWT_COLORS='root=blue,black
+	border=green,black
+	window=,black
+	button=black,green
+	sellistbox=green,black
+	actsellistbox=black,green
+	listbox=green,black
+	actlistbox=green,black
+	textbox=green,black
+	compactbutton=green,black'")
+
+# root : Background
+# border : Border
+# window : Window
+# textbox : Text
+# actsellistbox : List Selected
+# actlistbox : List Unselected
+# button : Button selected
+# compactbutton : Button unselected
+
+cmd+=('whiptail'  '--menu "Select Bookmark!" 20 60 10')
 
 
 while [ $i -ne $lim ];
@@ -20,4 +40,4 @@ cmd+=("3>&1 1>&2 2>&3")
 
 i=$(eval ${cmd[@]})
 i=$(($i - 1))
-#eval "cd $(readlink -f $LINKS${a[$i]})"
+eval "cd $(readlink -f $LINKS${a[$i]})"
